62e1ddb7937302ca5c2fe7e6af989c76eff1b133
Tomasz Lakomy <tomasz.lakomy@stxnext.pl>
2015-08-23 21:34:36 +0200

    Changed response type

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def hello():
     if request.method == 'POST':
-        return request.data
+        return request.values
     else:
         return 'Hello World!'
 
