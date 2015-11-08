540ba4c72831a45ead5dc351b8eadda2dab2861f
Tomasz Lakomy <tomasz.lakomy@stxnext.pl>
2015-11-08 13:01:33 +0100

    Fixed server response

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def hello():
     if request.method == 'POST':
-        return request.args.get('data', '')
+        return request.values.get('data', '')
     else:
         return 'Hello World!'
 
