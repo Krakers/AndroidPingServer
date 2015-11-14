23d1a75dd261e5832c92319bd8c799176bf3a8ad
Tomasz Lakomy <tomasz.lakomy@stxnext.pl>
2015-11-14 18:29:14 +0100

    Changed default socket server port

--- server_socket.py
+++ server_socket.py
@@ -5,7 +5,7 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('localhost', 1337)
+server_address = ('localhost', 8000)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
