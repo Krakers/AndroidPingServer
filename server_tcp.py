a60208adbfadc816eff848a67e78b74b28f895e4
Tomasz Lakomy <tomaszlakomy@yahoo.com>
2016-11-28 18:58:10 +0100

    Cleaned up tcp server

--- server_tcp.py
+++ server_tcp.py
@@ -8,21 +8,19 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the port
 port = int(sys.argv[1]) if len(sys.argv) > 1 else 8000
 server_address = ('0.0.0.0', port)
+
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
-
-# Listen for incoming connections
 sock.listen(1)
+# Set default packet size
 packet_size = 65000
 
 while True:
     # Wait for a connection
     print >>sys.stderr, 'waiting for a connection'
     connection, client_address = sock.accept()
-
     try:
         print >>sys.stderr, 'connection from', client_address
-
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(packet_size)
@@ -37,12 +35,7 @@ while True:
                 else:
                     print >>sys.stderr, 'sending data back to the client'
                     connection.send(data)
-            #else:
-            #    print >>sys.stderr, 'no more data from', client_address
-            #    break
-
     finally:
         # Clean up the connection
         print >>sys.stderr, 'closing the connection'
         connection.close()
-
